1752f7a5e8a18f86d2b91829365ba3ad09395131
clxchen <37133855+clxchen@users.noreply.github.com>
2018-03-07 18:36:18 +0800

    Update InterviewTest3.cpp

--- InterviewTest3.cpp
+++ InterviewTest3.cpp
@@ -14,7 +14,10 @@ bool duplicate(int numbers[],int length, int* duplication)
     while(numbers[i] != i)
     {
       if( numbers[i] = numbers[numbers[i]])
+      {
+        *duplication = numbers[i];
         return true;
+      }
       //交换两数
       int temp = numbers[i];
       numbers[i] = numbers[temp];
@@ -23,3 +26,48 @@ bool duplicate(int numbers[],int length, int* duplication)
   }
 }
 
+#include <iostream>
+using namespace std;
+//测试用例
+void test1(void)
+{
+    int vec[10] = {1,2,2,3,4,5,6,7,8,9};
+    int res;
+    auto f = duplicate(vec,10,&res);
+    if(false == f)
+        cout << "no duplication or input error" <<endl;
+    else
+        cout << "duplication number: " << res << endl;
+    
+}
+
+void test2(void)
+{
+    int vec[10] = {1,2,0,3,4,5,6,7,8,9};
+    int res;
+    auto f = duplicate(vec,10,&res);
+    if(false == f)
+        cout << "no duplication or input error" <<endl;
+    else
+        cout << "duplication number: " << res << endl;
+    
+}
+
+void test3(void)
+{
+    int vec[10] = {1,2,11,3,4,5,6,7,8,9};
+    int res;
+    auto f = duplicate(vec,10,&res);
+    if(false == f)
+        cout << "no duplication or input error" <<endl;
+    else
+        cout << "duplication number: " << res << endl;
+    
+}
+
+int main()
+{
+    test1();
+    test2();
+    test3();
+}
