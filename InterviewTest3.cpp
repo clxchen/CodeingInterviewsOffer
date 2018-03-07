ad3e5e9f23ff9c32f4e8b5a8ce66e02ae5088629
clxchen <37133855+clxchen@users.noreply.github.com>
2018-03-07 18:26:47 +0800

    Update InterviewTest3.cpp

--- InterviewTest3.cpp
+++ InterviewTest3.cpp
@@ -1,1 +1,25 @@
-aa
+//for finding the duplicate number
+
+bool duplicate(int numbers[],int length, int* duplication)
+{
+    if(numbers == nullptr || length <=0)
+      return false;
+    for(int i = 0; i < length;++i)
+    {
+      if(numbers[i] < 0 || numbers[i] >length - 1)
+        return false;
+    }
+  for(int i = 0; i< length ; ++i)
+  {
+    while(numbers[i] != i)
+    {
+      if( numbers[i] = numbers[numbers[i]])
+        return true;
+      //交换两数
+      int temp = numbers[i];
+      numbers[i] = numbers[temp];
+      numbers[temp] = temp;
+    }
+  }
+}
+
