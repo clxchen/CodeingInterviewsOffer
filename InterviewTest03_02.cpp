cfd1059d09560d2de357455bb33550b4a4336d2f
clxchen <37133855+clxchen@users.noreply.github.com>
2018-03-07 20:14:32 +0800

    Update InterviewTest03_02.cpp

--- InterviewTest03_02.cpp
+++ InterviewTest03_02.cpp
@@ -1,4 +1,5 @@
 
+
 int countRange(const int *numbers,int length,int start, int end);
 int getDuplication(const int *numbers,int length)
 {
@@ -9,14 +10,14 @@ int getDuplication(const int *numbers,int length)
   int end = length - 1;
   while(end >= start)
   {
-      int middle = ((start + end) >> 1) + start;
+      int middle = ((end - start) >> 1) + start;
       int count = countRange(numbers,length,start,middle);
       if(start == end)
       {
           if(count > 1)
             return count;
           else
-            return -1;
+            break;
       }
       
       if (count > (middle - start + 1))
@@ -32,7 +33,7 @@ int countRange(const int *numbers,int length,int start, int end)
     int count = 0;
     if(nullptr == numbers || length <= 0)
         return 0;
-    for(i = start; i < length; ++i )
+    for(int i = 0; i < length; ++i )
     {
         if (numbers[i] >= start && numbers[i] <= end)
           ++count;  
@@ -41,14 +42,18 @@ int countRange(const int *numbers,int length,int start, int end)
 }
 
 
+#include  <iostream>
+using namespace std;
+
+
 void test1()
 {
-    int a[10] = {1,2,2,4,5,6,7,8,9};
+    int a[10] = {1,2,2,4,5,6,7,8,9,9};
     auto res = getDuplication(a,10);
     if(res > 1)
-        cout << "find duplication" << endl;
+        cout << "test1:find duplication" << endl;
     else
-        cout << "no find duplication or input error" <<endl;
+        cout << "test1:no find duplication or input error" <<endl;
 }
 
 void test2()
@@ -66,9 +71,9 @@ void test3()
     int *a = nullptr;
     auto res = getDuplication(a,10);
     if(res > 1)
-        cout << "test2:find duplication" << endl;
+        cout << "test3:find duplication" << endl;
     else
-        cout << "test2:no find duplication or input error" <<endl;
+        cout << "test3:no find duplication or input error" <<endl;
 }
 
 int main()
@@ -77,5 +82,6 @@ int main()
     test2();
     test3();
 }
+
 
 
